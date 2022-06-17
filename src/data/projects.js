74160057043c09347746859f5d93a015b9ce20ab
Stephanie Shields <stephcshields@gmail.com>
2022-06-16 22:23:44 -0400

    Added project data

---
+++ src/data/projects.js
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    projectTitle: "USAspending.gov",
+    projectLink: "/work/usaspending-gov-project",
+    projectBadges: [`UX Design`, `UI Design`, `Front End Development`, `React`]
+  },
+  {
+    projectTitle: "FDMS.gov",
+    projectLink: "/work/fdms-gov-project",
+    projectBadges: [`UX Design`, `UI Design`, `Front End Development`, `Ember`]
+  },
+  {
+    projectTitle: "Recreation.gov",
+    projectLink: "/work/recreation-gov-project",
+    projectBadges: [`UI Design`, `Front End Development`, `React`]
+  },
+  {
+    projectTitle: "Regulations.gov",
+    projectLink: "/work/regulations-gov-project",
+    projectBadges: [`UX Design`, `UI Design`, `Front End Development`]
+  },
+  {
+    projectTitle: "myEverify.gov",
+    projectLink: "/work/myeverify-gov-project",
+    projectBadges: [`UI Design`, `Front End Development`]
+  }
+];
