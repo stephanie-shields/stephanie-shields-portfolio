fcaba373ee8bec5a2f909fe5363826ace7c050b4
Stephanie Shields <stephcshields@gmail.com>
2023-10-26 15:48:28 -0400

    Updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,21 +3,34 @@ module.exports = {
     title: `Stephanie Shields Portfolio`,
     siteUrl: `https://stephanie-shields.github.io/stephanie-shields-portfolio`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: ['gatsby-source-filesystem', 'gatsby-omni-font-loader'],
-    options: {
-      "name": "images",
-      "path": "./src/images/",
-      enableListener: true,
-      preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
-      web: [
-        {
-          name: `PT Serif`,
-          file: `https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap`,
-        },
-      ],
+  plugins: [
+    "gatsby-plugin-sass", 
+    "gatsby-plugin-image", 
+    "gatsby-plugin-react-helmet", 
+    "gatsby-plugin-sitemap", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/",
+      },
+      __key: "images"
     },
-    __key: "images"
-  }],
+    {
+      resolve: 'gatsby-omni-font-loader',
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `PT Serif`,
+            file: `https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap`,
+          },
+        ],
+      },
+    }
+  ],
   pathPrefix: "/stephanie-shields-portfolio"
 };
