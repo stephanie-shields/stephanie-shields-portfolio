d011aa0a52bd51b80dcc9a3406cd3461d7ad64c0
Stephanie Shields <stephcshields@gmail.com>
2023-10-26 15:02:36 -0400

    Added gatsby-browser and adjusted global styles

---
+++ gatsby-browser.js
@@ -0,0 +1,4 @@
+import "@fontsource/pt-serif/400.css";
+import "@fontsource/pt-serif/700.css";
+import "@fontsource-variable/inter";
+import "./src/styles/global.scss";
--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Badge = ({ badgeText, badgeBackround }) => {
   const hasBadgeBackround = badgeBackround ? badgeBackround : `text-bg-light`;
   return (
-    <span className={`badge border ${hasBadgeBackround}`}>{badgeText}</span>
+    <span className={`badge border border-dark-subtle ${hasBadgeBackround}`}>{badgeText}</span>
   )
 }
 
